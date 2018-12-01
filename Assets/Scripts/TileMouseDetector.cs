79fd3434e7125c467487b0119ff6ddfe2f10cfe7
JulesPomme <julian.moreira.fr@gmail.com>
2018-12-01 17:59:15 +0100

    Map Generation + Selector

---
+++ Assets/Scripts/TileMouseDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMouseDetector : MonoBehaviour {
+
+    private bool mouseIsOver;
+    public bool MouseIsOver { get { return mouseIsOver; } }
+
+    void Start() {
+        mouseIsOver = false;
+    }
+
+    private void OnMouseOver() {
+        mouseIsOver = true;
+    }
+
+    private void OnMouseExit() {
+        mouseIsOver = false;
+    }
+}
