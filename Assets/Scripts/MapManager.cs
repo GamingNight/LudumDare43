c2e2ca418853a8e1bd975cf797baad916deaa521
JulesPomme <julian.moreira.fr@gmail.com>
2018-12-01 19:29:28 +0100

    Rain Script

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -7,9 +7,7 @@ public class MapManager : MonoBehaviour {
     public int mapSize = 5;
     public float tileHeight = 4;
     public float tileWidth = 5;
-    public GameObject defaultTilePrefab;
-    public GameObject tile0Prefab;
-    public GameObject tile100Prefab;
+    public GameObject tilePrefab;
     public GameObject selectorPrefab;
 
     private GameObject[][] map;
@@ -32,12 +30,36 @@ public class MapManager : MonoBehaviour {
         for (int i = 0; i < mapSize; i++) {
             map[i] = new GameObject[mapSize];
             for (int j = 0; j < mapSize; j++) {
-                GameObject defaultTileClone = Instantiate<GameObject>(defaultTilePrefab);
-                defaultTileClone.transform.position = new Vector3(initPosX - i * tileWidth / 2 + j * tileWidth / 2, initPosY - i * tileHeight / 2 - j * tileHeight / 2, 0);
-                defaultTileClone.transform.parent = gameObject.transform;
-                map[i][j] = defaultTileClone;
+                GameObject tileClone = Instantiate<GameObject>(tilePrefab);
+                tileClone.transform.position = new Vector3(initPosX - i * tileWidth / 2 + j * tileWidth / 2, initPosY - i * tileHeight / 2 - j * tileHeight / 2, 0);
+                tileClone.transform.parent = gameObject.transform;
+                tileClone.name = tilePrefab.name + "" + i + "" + j;
+                map[i][j] = tileClone;
             }
         }
+
+        InitTileData();
+    }
+
+    private void InitTileData() {
+
+        List<int> rainValues = new List<int>();
+        for (int i = 0; i < mapSize * mapSize; i++) {
+            if (i == 0) {
+                rainValues.Add(130);
+            } else {
+                rainValues.Add(rainValues[i - 1] + 20);
+            }
+        }
+
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
+                int pickedIndex = Random.Range(0, rainValues.Count);
+                map[i][j].GetComponent<TileData>().initRainValue = rainValues[pickedIndex];
+                rainValues.RemoveAt(pickedIndex);
+            }
+        }
+
     }
 
     void Update() {
---
+++ Assets/Scripts/TileData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour {
+
+    public Sprite[] rainSprites;
+    public int initRainValue = 100;
+    public int[] rainStages = { 100 };
+    /// <summary>
+    /// Data drops at rate *this per second*
+    /// </summary>
+    public float dataDropRate = 0.1f;
+
+    private SpriteRenderer spriteRenderer;
+    private float rainValue;
+
+    void Start() {
+        rainValue = initRainValue;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+
+        rainValue = Mathf.Max(0, rainValue - (Time.deltaTime * dataDropRate));
+        int i = 0;
+        while (i < rainStages.Length && rainValue > rainStages[i]) {
+            i++;
+        }
+        spriteRenderer.sprite = rainSprites[i];
+    }
+}
