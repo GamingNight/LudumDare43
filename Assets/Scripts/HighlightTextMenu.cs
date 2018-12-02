501745fb34fd7c5463765ec4cb9de3ab6b40936a
JulesPomme <julian.moreira.fr@gmail.com>
2018-12-03 00:16:37 +0100

    start menu + cursor

---
+++ Assets/Scripts/HighlightTextMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightTextMenu : MonoBehaviour {
+
+    public Sprite selectedSprite;
+    public Sprite unselectedSprite;
+
+    private bool selected;
+    public bool Selected { get { return selected; } }
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start() {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseEnter() {
+        spriteRenderer.sprite = selectedSprite;
+        selected = true;
+    }
+
+    private void OnMouseExit() {
+        spriteRenderer.sprite = unselectedSprite;
+        selected = false;
+    }
+}
---
+++ Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour {
+
+    public HighlightTextMenu startMenuHighlight;
+    public HighlightTextMenu quitMenuHighlight;
+    public Texture2D cursor;
+
+    private void Start() {
+
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (startMenuHighlight.Selected)
+                SceneManager.LoadScene("main");
+            else if (quitMenuHighlight.Selected) {
+                Application.Quit();
+            }
+        }
+    }
+}
