9ce347845e3e991e8047ca680832849fab5fafb8
JulesPomme <julian.moreira.fr@gmail.com>
2018-12-03 14:53:51 +0100

    gameover scenario v2

--- Assets/Scripts/GameOverScenario.cs
+++ Assets/Scripts/GameOverScenario.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverScenario : MonoBehaviour {
 
     public enum GameOverState {
         DEZOOM_CAMERA, DISPLAY_STATS, NONE
     }
+
+    public SpriteRenderer gameOverSprite;
+    public SpriteRenderer scoreSprite;
+    public SpriteRenderer bonusSprite;
+    public SpriteRenderer retrySprite;
+    public SpriteRenderer quitSprite;
 
     private GameOverState state;
 
@@ -17,8 +24,12 @@ public class GameOverScenario : MonoBehaviour {
 
     void Update() {
 
-        if(state == GameOverState.DEZOOM_CAMERA) {
+        if (state == GameOverState.DEZOOM_CAMERA) {
             StartCoroutine(AdjustCameraCoroutine(2f, 8.5f, 5.5f));
+            state = GameOverState.NONE;
+        } else if (state == GameOverState.DISPLAY_STATS) {
+            //StartCoroutine(DisplayHUD(2f));
+            state = GameOverState.NONE;
         }
     }
 
@@ -27,12 +38,27 @@ public class GameOverScenario : MonoBehaviour {
         float initZoom = Camera.main.orthographicSize;
         Vector3 initPos = Camera.main.transform.position;
         float step = 0;
-        while(step < lerpDuration) {
+        while (step < lerpDuration) {
             Camera.main.orthographicSize = Mathf.Lerp(initZoom, targetCameraZoom, step / lerpDuration);
             Camera.main.transform.position = new Vector3(Mathf.Lerp(initPos.x, targetXPosition, step / lerpDuration), initPos.y, initPos.z);
             step += 0.01f;
             yield return new WaitForSeconds(0.01f);
         }
         state = GameOverState.DISPLAY_STATS;
+    }
+
+    private IEnumerator DisplayHUD(float lerpDuration) {
+
+        float step = 0;
+        while (step < lerpDuration) {
+            float lerp = Mathf.Lerp(0, 1, step / lerpDuration);
+            gameOverSprite.color = new Color(1, 1, 1, lerp);
+            scoreSprite.color = new Color(1, 1, 1, lerp);
+            bonusSprite.color = new Color(1, 1, 1, lerp);
+            retrySprite.color = new Color(1, 1, 1, lerp);
+            quitSprite.color = new Color(1, 1, 1, lerp);
+            step += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
